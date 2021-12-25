c1cdcabbacea501c03dd9d6d59293ac06ce85a7f
spanda <santanu.panda@gcp.lowes.com>
2021-12-25 22:28:36 +0530

    useEffect as replacement for componentWillUnmount i.e. cleanup code

--- effect-hook/src/App.js
+++ effect-hook/src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 // import ClassCounterOne from "./components/ClassCounterOne";
-import HookCounterOne from "./components/HookCounterOne";
+// import HookCounterOne from "./components/HookCounterOne";
+// import ClassMouse from "./components/ClassMouse";
+// import HookMouse from "./components/HookMouse";
+import MouseContainer from "./components/MouseContainer";
 
 function App() {
   return (
     <div className='App'>
       {/* <ClassCounterOne /> */}
-      <HookCounterOne />
+      {/* <HookCounterOne /> */}
+      {/* <ClassMouse /> */}
+      {/* <HookMouse /> */}
+      <MouseContainer />
     </div>
   );
 }
--- effect-hook/src/components/HookCounterOne.js
+++ effect-hook/src/components/HookCounterOne.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from "react";
 
 function HookCounterOne() {
   let [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
   useEffect(() => {
+    console.log("useEffect - updating document content");
     document.title = `You clicked ${count} times`;
-  });
+  }, [count]);
 
   return (
     <div>
+      <input type='text' value={name} onChange={e => setName(e.target.value)} />
       <button onClick={() => setCount(count + 1)}>Count {count} times</button>
     </div>
   );
